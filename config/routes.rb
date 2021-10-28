cd3829dbcd9a634e01ef9138f1c13fd0d1398519
hiro.oshiro <longstraw52@gmail.com>
2021-10-28 14:14:59 +0000

    Add user signup function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/', to: 'users#index'
+  root to: 'toppages#index'
   get 'signup', to: 'users#new'
-  resources :users, exept: [:index, :new] 
+  resources :users, exept: [:new]
 end
